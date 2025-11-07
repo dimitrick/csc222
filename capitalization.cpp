99965146d01a74da151d5d35d3ba342b2af9a543
dimitrick <dck65455@email.vccs.edu>
2025-11-06 22:39:07 -0500

    removed headers

--- capitalization.cpp
+++ capitalization.cpp
@@ -5,13 +5,8 @@ using namespace std;
 string toUpper(string s) {
     string newString = "";
     for (int i = 0; i < s.length(); i++) {
-<<<<<<< HEAD:test4.cpp
         if (s[i] >= 'a' && s[i] <= 'z') {
             newString += s[i] - ('a' - 'A');
-=======
-        if (s[i] >= 97) {
-            newString += s[i] - 32;
->>>>>>> d7e06b2195c1fa3fb0290610dde9348975ab948e:capitalization.cpp
         } else {
             newString += s[i];
         }
@@ -22,13 +17,8 @@ string toUpper(string s) {
 string toLower(string s) {
     string newString = "";
     for (int i = 0; i < s.length(); i++) {
-<<<<<<< HEAD:test4.cpp
         if (s[i] >= 'A' && s[i] <= 'Z') {
             newString += s[i] + ('a' - 'A');
-=======
-        if (s[i] <= 97) {
-            newString += s[i] + 32;
->>>>>>> d7e06b2195c1fa3fb0290610dde9348975ab948e:capitalization.cpp
         } else {
             newString += s[i];
         }
