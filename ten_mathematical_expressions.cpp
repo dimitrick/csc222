f0a4d99d3dfb6011cf15d4b9567aad35ea68c2e1
dimitrick <dck65455@email.nvcc.edu>
2025-12-17 13:56:56 -0500

    Clean up and add some files

---
+++ ten_mathematical_expressions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 5;
+	int m = 7;
+	cout << "The product of " << n << " and " << m << " is ";
+	cout << n * m << "." << endl;
+	cout << "The sum of " << n << " and " << m << " is ";
+	cout << n + m << "." << endl;
+	cout << "The quotient of " << n << " and " << m << " is ";
+	cout << n / m << "." << endl;
+	cout << "The difference of " << m  << " and " << n << " is ";
+	cout << m - n << "." << endl;
+	
+}
